6ffff4885057347d16159768631a327932060d83
Recxo <rishava065@gmail.com>
2023-09-04 22:09:48 +0530

    commit

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
+    slug = models.SlugField(_(""))
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, related_name='participants',blank=True)
     updated = models.DateTimeField(auto_now=True)
--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
-    path('profile/<str:pk>',views.userProfile,name='user-profile'),
+    path('profile/<str:pk>/',views.userProfile,name='user-profile'),
 
 
 
